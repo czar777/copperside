56780b4431ab2c260afba886bd607cf95cc7bd07
Sergey Gerasimov <czar997@yandex.ru>
2023-12-13 16:29:37 +0300

    добавлена контроллер на конвертер json в XML

---
+++ src/main/java/com/test/copperside/controller/JsonToXmlController.java
@@ -0,0 +1,16 @@
+package com.test.copperside.controller;
+
+import com.github.underscore.U;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JsonToXmlController {
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_XML_VALUE)
+    public String jsonToXml(@RequestBody String json) {
+        return U.jsonToXml(json, U.JsonToXmlMode.REMOVE_ATTRIBUTES);
+    }
+}
