ae0d7ba108dc78821ef88b8f8351b735f8a2e18a
Sergey Gerasimov <czar997@yandex.ru>
2023-12-13 14:07:04 +0300

    init

---
+++ src/main/java/com/test/copperside/CoppersideApplication.java
@@ -0,0 +1,13 @@
+package com.test.copperside;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoppersideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoppersideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/copperside/CoppersideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.copperside;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoppersideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
